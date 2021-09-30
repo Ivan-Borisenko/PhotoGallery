879c65e2cb37a1b164daf7695ce17d2fe146944c
Ivan-Borisenko <ivan.v.borisenko@gmail.com>
2021-09-30 22:32:40 +0300

    Retrofit with APIkey, ViewModel and LiveData with Observer, RecyclerView

--- app/src/main/java/com/example/photogallery/api/FlickrApi.kt
+++ app/src/main/java/com/example/photogallery/api/FlickrApi.kt
@@ -8,9 +8,9 @@ interface FlickrApi {
     @GET(/*"/")
     fun fetchContents(): Call<String>*/
         "servises/rest/?method=flickr.interestingness.getList" +
-                "@&api_key=_Api_" +
+                "@&api_key=e6e28bca87ed6ee5ece0cbc9effe783f" +
                 "&nojsoncallback=1" +
                 "&extras=url_s"
     )
-    fun fetchPhotos(): Call<String>
+    fun fetchPhotos(): Call<FlickrResponse>
 }
